6fdc3977fd5dbc7b5015f2d0de522377a220e96c
Heytemisan <temyicy@gmail.com>
2022-05-09 14:02:23 +0100

    feat: created a function for reordering card positions

--- src/state/boardData.jsx
+++ src/state/boardData.jsx
@@ -2,6 +2,7 @@ import { Subject } from "rxjs";
 import uuid from "uuidv4";
 
 //a subject is an observable that talks to many observers
+
 let boardData = null;
 export const dataSubject = new Subject();
 
@@ -9,7 +10,7 @@ dataSubject.subscribe((_boardData) => {
 	boardData = _boardData;
 });
 
-//moves a random piece on the board
+//moves a random piece within the board
 export const reorderListPosition = (initialPosition, newPosition) => {
 	Object.values(boardData).forEach((list) => {
 		if (list.position === initialPosition) {
@@ -30,3 +31,51 @@ export const reorderListPosition = (initialPosition, newPosition) => {
 	});
 	dataSubject.next({ ...boardData });
 };
+
+export const reorderCardPosition = (source, destination, cardId) => {
+	//moves a random piece between same cards positions
+	if (source.droppableId === destination.droppableId) {
+		const { cards } = boardData[source.droppableId];
+
+		Object.values(cards).forEach((card => {
+			if (card.position === source.index) {
+				card.position = destination.index;
+				return;
+			}
+			if (card.position < Math.min(source.index, destination.index) ||
+				card.position> Math.max(source.index, destination.index)
+			) {
+				return;
+			} 
+			if (source.index < destination.index) {
+				card.position--;
+				return;
+			}
+			card.position++;
+		}))
+	}
+	//moving a random piece between different cards positions
+	else {
+		const sourceCards = boardData[source.droppableId].cards;
+		const destinationCards = boardData[destination.droppableId].cards;
+		const movingCards = boardData[source.droppableId].cards[cardId];
+
+		Object.values(sourceCards).forEach(card => {
+			if (card.position >= source.index) {
+				card.position--;
+			}
+		});
+
+		Object.values(destinationCards).forEach(card => {
+			if (card.position >= destination) {
+				card.position++;
+			}
+		});
+		
+		delete boardData[source.droppableId].cards[cardId];
+		movingCards.position = destination.index;
+		boardData[destination.droppableId].cards[cardId] = movingCards;
+	}
+
+	dataSubject.next({...boardData})
+}
