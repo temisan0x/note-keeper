909bc79bdf581e06423f57a4075b0cb1fdfcb370
Heytemisan <temyicy@gmail.com>
2022-05-09 15:32:02 +0100

    chore: code cleanup

---
+++ src/components/notes/NoteTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { updateListTitle, deleteList } from '../../state/boardData';
+
+const NoteTitle = () => {
+    return (
+        <div>NoteTitle</div>
+    )
+}
+
+export default NoteTitle
